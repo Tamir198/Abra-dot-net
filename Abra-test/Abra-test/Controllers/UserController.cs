13f12561992f09b2690bd86d12ef9741b84b58b0
Tamir <1tamir198@gmail.com>
2023-03-08 11:41:37 +0200

    Finished the first part

---
+++ Abra-test/Abra-test/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+      using Newtonsoft.Json.Linq;
+        using System.Text.Json;
+
+namespace Abra_test.Controllers {
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : Controller {
+        private readonly HttpClient _httpClient;
+
+        public UserController(HttpClient httpClient) {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet("GetUsersData")]
+        public async Task<IActionResult> GetUsersData([FromQuery] string gender) {
+            String url = "https://randomuser.me/api/?results=10&gender=" + gender;
+                var response = await _httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode) {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return Ok(responseContent);
+                }
+                else {
+                    return StatusCode((int)response.StatusCode);
+                }
+         }
+
+        [HttpGet("GetMostPopularCountry")]
+        public async Task<IActionResult> GetMostPopularCountry() {
+            var response = await _httpClient.GetAsync("https://randomuser.me/api/?results=5000");
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var json = JObject.Parse(responseContent);
+
+            var countries = json["results"]
+                .Select(result => result["location"]["country"].ToString())
+                .GroupBy(country => country)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            var mostPopularCountry =  countries.OrderByDescending(pair => pair.Value).First().Key;
+
+
+            return Ok(mostPopularCountry);
+
+        }
+
+        [HttpGet("GetListOfMails")]
+        public async Task<IActionResult> GetListOfMails() {
+            var response = await _httpClient.GetAsync("https://randomuser.me/api/?results=30");
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var json = JObject.Parse(responseContent);
+
+
+            var emails = json["results"]
+                .Select(result => result["email"].ToString())
+                .ToList();
+     
+
+            return Ok(emails);
+
+        }
+
+
+        [HttpGet("GetTheOldestUser")]
+        public async Task<IActionResult> GetTheOldestUser() {
+            var response = await _httpClient.GetAsync("https://randomuser.me/api/?results=100");
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var json = JObject.Parse(responseContent);
+
+            var oldestUser = json["results"]
+             .Select(result => new {
+            Name = result["name"]["first"].ToString() + " " + result["name"]["last"].ToString(),
+            Age = result["dob"]["age"].ToObject<int>()
+        })
+        .OrderByDescending(user => user.Age)
+        .FirstOrDefault();
+
+
+            return Ok(oldestUser);
+
+        }
+
+
+
+    }
+}
--- Abra-test/Abra-test/Program.cs
+++ Abra-test/Abra-test/Program.cs
@@ -1,8 +1,12 @@
+using Abra_test.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<UserController>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
